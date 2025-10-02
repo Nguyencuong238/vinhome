fd32aeafe8987b3ad8d279e1055e6b0525f9508b
cuongnv238 <nguyencuong23893@gmail.com>
2025-10-02 09:09:16 +0700

    update

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -273,7 +273,13 @@
             box-shadow: 0 8px 24px rgba(0, 168, 150, 0.3);
         }
     </style>
-    
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WK9SW2KW');</script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body>
