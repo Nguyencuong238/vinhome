6d65448051c08e21f3a28e72a5cb55350e6d76b0
cuongnv238 <nguyencuong23893@gmail.com>
2025-10-03 16:16:54 +0700

    update

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -53,9 +53,7 @@
     <link rel="stylesheet" href="{{ asset('assets/styles.css') . '?v=' . config('app.asset_version') }}">
     <link rel="stylesheet" href="{{ asset('assets/utilities.css') . '?v=' . config('app.asset_version') }}">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Playfair+Display:wght@700;800;900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     @yield('css')
 
     <style>
