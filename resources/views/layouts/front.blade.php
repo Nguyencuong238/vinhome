68cf78e4b6605d5596b09d3be402afe621c4f3e3
cuongnv238 <nguyencuong23893@gmail.com>
2025-10-09 09:18:37 +0700

    update

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -281,6 +281,11 @@
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WK9SW2KW"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
     <!-- Top Bar -->
     <div class="top-bar">
         <div class="container">
