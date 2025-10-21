66ea368a1522ecac4b268fe1d391bd7dc595e734
cuongnv238 <nguyencuong23893@gmail.com>
2025-10-21 16:12:57 +0700

    update

--- resources/views/backend/newsletters/index.blade.php
+++ resources/views/backend/newsletters/index.blade.php
@@ -18,6 +18,7 @@
                         <th>#</th>
                         <th>{{ __('Name') }}</th>
                         <th>{{ __('Email') }}</th>
+                        <th>{{ __('Số điện thoại') }}</th>
                         <th>{{ __('Quan tâm') }}</th>
                         <th>{{ __('Message') }}</th>
                         <th>{{ __('Date') }}</th>
@@ -30,6 +31,7 @@
                         <td>{{ $loop->iteration }}</td>
                         <td>{{ $contact->name }}</td>
                         <td>{{ $contact->email }}</td>
+                        <td>{{ $contact->phone }}</td>
                         <td>{{ $contact->product }}</td>
                         <td>{{ $contact->message }}</td>
                         <td>{{ $contact->created_at->format('d/m/Y H:i:s') }}</td>
